5f090e305b64b3cd40c1026c46fa13d57aced59c
teodor russu <teodor_russu@yahoo.com>
2022-10-01 19:56:20 +0200

    * update tags for subnet * add outputs for development vpc and development subnet 1 arns

--- main.tf
+++ main.tf
@@ -8,4 +8,16 @@ resource "aws_vpc" "development-vpc"{
 resource "aws_subnet" "dev-subnet-1" {
   cidr_block = "10.0.10.0/24"
   vpc_id     = aws_vpc.development-vpc.id
+  tags = {
+    "Name"="development-subnet-1"
+  }
 }
+
+output "development-vpc-arn"{
+  value = aws_vpc.development-vpc.arn
+}
+
+output "development-subnet-1-arn"{
+  value = aws_subnet.dev-subnet-1.arn
+}
+
